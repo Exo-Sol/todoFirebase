8a0455745fd3607849f485fe7327734587d607cd
Exo Sol <1.petarzlatar@gmail.com>
2024-05-29 21:28:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const initialTodos = [
+  {
+    id: 1,
+    title: "Go grocery shopping",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "Be more humble",
+    done: true,
+  },
+  {
+    id: 3,
+    title: "Walk the dog",
+    done: false,
+  },
+];
+
+const App = () => {
+  const [todosArray, setTodosArray] = useState(initialTodos);
+  const [todo, setTodo] = useState("");
+
+  const togleTodo = (todo) => {
+    setTodosArray(
+      todosArray.map((t) => {
+        return t.id === todo.id ? { ...t, done: !t.done } : t;
+      })
+    );
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setTodosArray([
+      ...todosArray,
+      { id: Date.now(), title: todo, done: false },
+    ]);
+  };
+
+  const deleteTodo = (tt) => {
+    console.log(tt);
+
+    todosArray.forEach((ele, ind) => {
+      if (tt === ele.id) {
+        let newArray = todosArray.toSpliced(ind, 1);
+
+        return setTodosArray(newArray);
+      } else {
+        return ele;
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <h1>TODO Tracker</h1>
       </header>
+      <form
+        onSubmit={(e) => submitForm(e)}
+        onKeyDown={(e) => {
+          e.key === "eneter" && submitForm();
+        }}
+      >
+        <input
+          value={todo}
+          placeholder="write new todo"
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <button>add</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setTodo("");
+          }}
+        >
+          clear
+        </button>
+      </form>
+      <main className="main">
+        {todosArray.map((todo) => (
+          <div>
+            <div
+              key={todo.id}
+              className="todo"
+              style={{ textDecoration: todo.done ? "line-through" : undefined }}
+              onClick={() => togleTodo(todo)}
+            >
+              {todo.title}
+            </div>
+            <button onClick={() => deleteTodo(todo.id)}>delete</button>
+          </div>
+        ))}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
